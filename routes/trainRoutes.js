08a431949ae7427a2988c991c013d50c16ec1149
Sajal Vasisht <sajalvasisht06@gmail.com>
2026-03-02 16:13:32 +0530

    Update trainRoutes.js

--- routes/trainRoutes.js
+++ routes/trainRoutes.js
@@ -4,8 +4,5 @@ const router = express.Router();
 router.get("/", (req, res) => {
     res.send("Train route working");
 });
-router.get("/error-test", (req, res) => {
-    throw new Error("Testing global error handler");
-});
 
 module.exports = router;
